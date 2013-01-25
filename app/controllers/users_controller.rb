1a3a4c32700ff9b56a543e5ad92c53d65f368204
Your Name <gregoryehill@gmail.com>
2013-01-25 10:07:06 -0600

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+ def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -18,3 +19,4 @@ class UsersController < ApplicationController
     end
   end
 end
+
