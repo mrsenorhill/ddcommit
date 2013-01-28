013afff390076159ae0086c38eb26e7a0fc47d0b
Your Name <gregoryehill@gmail.com>
2013-01-28 12:46:55 -0600

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -52,6 +52,18 @@ describe "authorization" do
           click_button "Sign in"
         end
       
+        describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }          
+        end
+      end
+      
       describe "after signing in" do
 
           it "should render the desired protected page" do
@@ -90,7 +102,16 @@ describe "authorization" do
           it { should have_selector('title', text: 'Sign in') }
         end
       end
-    end
+       describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+      end
 
 
 describe "as wrong user" do
